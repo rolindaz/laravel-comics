0f3fb27f70c75fb6aa019f9791e4beb6e63b5650
rolindaz <rolinda.zamboni@gmail.com>
2025-12-09 15:43:58 +0100

    feat: add DC logo and update header layout in Laravel Comics project

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@ $navitems = config('navitems')
         <header class='bg-white text-black text-uppercase'>
             <div class="container h-100 d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <Logo />
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC">
                 </div>
                 <nav class='h-100'>
                     <ul class='h-100 d-flex gap-4 list-unstyled mb-0 align-items-center'>

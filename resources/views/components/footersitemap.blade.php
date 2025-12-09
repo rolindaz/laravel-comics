a8ffa76fa1f8523fd93f692a618ca514a202ced2
rolindaz <rolinda.zamboni@gmail.com>
2025-12-09 17:20:00 +0100

    feat: add footer sitemap component with dynamic content and DC logo

--- resources/views/components/footersitemap.blade.php
+++ resources/views/components/footersitemap.blade.php
@@ -1,3 +1,56 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-</div>
+@php
+    $websiteSections = config('websiteSections')
+@endphp
+
+<div class="footer-sitemap position-relative overflow-hidden">
+            <div class="container py-4">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo DC" class="z-3 position-absolute" />
+                <div class="row">
+                    @foreach ($websiteSections as $index => $section)
+                    @if ($index != 3)
+                        <div class="col">
+                        <h5 class="text-white text-uppercase fw-bold">
+                           {{ $section['title'] }}
+                        </h5>
+                        <ul class="list-unstyled">
+                            @foreach ($section['content'] as $content)
+                            <li>
+                                <a href="{{ Vite::asset($content['link']) }}" class="text-decoration-none">
+                                   {{ $content['text'] }}
+                                </a>
+                            </li>
+                            @endforeach
+                        </ul>
+
+                        {{-- {(index == 0) ? (
+                            websiteSections.map((section, index) => {
+                                if (index == 3) {
+                                    return (
+                                        <div class="col" key={section.id}>
+                                            <h5 class="text-white text-uppercase fw-bold">
+                                                {section.title}
+                                            </h5>
+                                            <ul class="list-unstyled">
+                                                {
+                                                    section.content.map((item) => {
+                                                        return (
+                                                            <li key={item.id}>
+                                                                <a href={item.link} class="text-decoration-none">
+                                                                    {item.text}
+                                                                </a>
+                                                            </li>
+                                                        )
+                                                    })
+                                                }
+                                            </ul>
+                                        </div>
+                                    )
+                                }
+                            })
+                        ) : ''} --}}
+                    </div>
+                    @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>

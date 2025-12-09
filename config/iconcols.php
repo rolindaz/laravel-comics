16fc598bc07ad177281bef1190296b5e1e5d14d1
rolindaz <rolinda.zamboni@gmail.com>
2025-12-09 16:06:37 +0100

    feat: add icon columns configuration and update banner component to display icons with titles

---
+++ config/iconcols.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    [
+        'id' => 1,
+        'image' => './', // Replace with the actual path or variable
+        'title' => 'Digital Comics'
+    ],
+    [
+        'id' => 2,
+        'image' => 'merchandiseIcon', // Replace with the actual path or variable
+        'title' => 'DC Merchandise'
+    ],
+    [
+        'id' => 3,
+        'image' => 'subscriptionIcon', // Replace with the actual path or variable
+        'title' => 'Subscription'
+    ],
+    [
+        'id' => 4,
+        'image' => 'shopLocatorIcon', // Replace with the actual path or variable
+        'title' => 'Comic Shop Locator'
+    ],
+    [
+        'id' => 5,
+        'image' => 'dcVisaIcon', // Replace with the actual path or variable
+        'title' => 'DC Power Visa'
+    ]
+];
--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+    @php
+        $iconcols = config('iconcols')
+    @endphp
+    
+    <div class="banner text-white text-uppercase">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-around px-3">
+                    @foreach ($iconcols as $iconcol)
+                    <div class="col text-align-center">
+                        <img class="h-100 me-2" src="{{ $iconcol['image'] }}" alt="{{ $iconcol['image'] }}" />
+                        <span class="align-middle">
+                           {{ $iconcol['title'] }}
+                        </span>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>

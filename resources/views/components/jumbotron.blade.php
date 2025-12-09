e9a69ccf75037ecc70198e57980109b73af57cec
rolindaz <rolinda.zamboni@gmail.com>
2025-12-09 16:34:21 +0100

    feat: add comic card and grid components; implement jumbotron and update home view

--- resources/views/components/jumbotron.blade.php
+++ resources/views/components/jumbotron.blade.php
@@ -1,3 +1,2 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
-</div>
+<section class='jumbotron'>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,8 @@
 
 @section('main')
 <main class="bg-black text-white fw-bold">
-            {{-- <Jumbotron /> --}}
-            <div class="container d-flex align-items-center h-100 pb-2 pt-4 position-relative flex-column justify-content-center gap-3">
-                <div class='text-white text-uppercase fw-bold position-absolute top-0 translate-middle current-series py-2 px-3'>
-                    Current series
-                </div>
-                <div class="row row-cols-sm-2 row-cols-md-4 row-cols-lg-6 h-100 justify-content-around py-3 row-gap-3">
-                    {
-                        comics.map(({ id, title, thumb, series }) => {
-                            return (
-                                <ComicCard key={id} src={thumb} alt={title} spanContent={series} />
-                            )
-                        })
-                    }
-                </div>
-                <button class="border-0 rounded-0 text-white text-uppercase fw-bold py-2 px-4 mb-4">
-                    Load more
-                </button>
-            </div>
+            <x-jumbotron></x-jumbotron>
+            <x-comic-grid></x-comic-grid>
             <x-banner></x-banner>
         </main>
 @endsection
